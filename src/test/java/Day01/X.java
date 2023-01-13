d84d827b9048b8d2a18a746cf117116ffd395bf5
Nihal <engnihal19@gmail.com>
2023-01-13 21:51:53 +0100

    changes done

--- src/test/java/Day01/X.java
+++ src/test/java/Day01/X.java
@@ -2,6 +2,6 @@ package Day01;
 
 public class X {
     public static void main(String[] args) {
-        System.out.println("You can achieve");
+        System.out.println("You can reach your goal");
     }
 }
