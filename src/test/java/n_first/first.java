0061cd3dce80bba39b709ab8d36a6e9a58acbaec
Ömer <lgoh597@gmail.com>
2023-01-13 21:22:17 +0100

    n.deneme

---
+++ src/test/java/n_first/first.java
@@ -0,0 +1,7 @@
+package n_first;
+
+public class first {
+    public static void main(String[] args) {
+        System.out.println("h");
+    }
+}
---
+++ src/test/java/n_first/second.java
@@ -0,0 +1,7 @@
+package n_first;
+
+public class second {
+    public static void main(String[] args) {
+        System.out.println("w");
+    }
+}
