a541a6b5794b8010f8f804ecb8b393e720138a0f
Gurcan9159 <gurcansevkican@gmail.com>
2023-01-13 22:14:38 +0100

    Hello Max

--- src/test/java/gurcann/gurcans.java
+++ src/test/java/gurcann/gurcans.java
@@ -2,6 +2,6 @@ package gurcann;
 
 public class gurcans {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("Hello Max whats up");
     }
 }
