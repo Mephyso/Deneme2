47a9fe712070797b63311f60de226e7a2ed3a2d2
Gurcan9159 <gurcansevkican@gmail.com>
2023-01-13 21:49:48 +0100

    added2..

--- src/test/java/gurcan/grcn.java
+++ src/test/java/gurcan/grcn.java
@@ -2,6 +2,6 @@ package gurcan;
 
 public class grcn {
     public static void main(String[] args) {
-        System.out.println("Hello Max");
+        System.out.println("Hello Max whats up");
     }
 }
