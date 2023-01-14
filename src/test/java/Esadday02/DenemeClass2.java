c6a1d8dea50476bbb8a96478b2355127316cfec0
Esad <yildizme4@gmail.com>
2023-01-14 10:39:18 +0100

    Bu notu 14.01.2023'te yaziyorum :)

---
+++ src/test/java/Esadday02/DenemeClass2.java
@@ -0,0 +1,22 @@
+package Esadday02;
+
+public class DenemeClass2 {
+    public static void main(String[] args) {
+        int a = 23;
+
+        String b = "Lütfen kurallara uyunuz";
+
+        String c = "Grup dinamigi cok onemlidir";
+
+        System.out.println(a);
+
+        System.out.println(b);
+
+        System.out.println(c);
+
+
+    }
+
+
+
+}
